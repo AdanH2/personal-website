d46b661ff0e749fd6193c9e39d2e58613776bffd
Adan Hernandez <adanh74379@gmail.com>
2025-03-14 01:16:58 -0700

    Finished with the Navbar and setting up the rest of the pages. Made some progress on home page.

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+document.getElementById("navObject").addEventListener("load", function () {
+  const navDoc = this.contentDocument || this.contentWindow.document;
+  const links = Array.from(navDoc.querySelectorAll("a.nav-link"));
+  const navbarBrand = navDoc.querySelector("a.navbar-brand");
+  links.push(navbarBrand);
+  console.log(links);
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      window.location.href = this.href;
+    });
+  });
+});
